aeaf80978c61548107386c4163762ab8d5e2c080
DustyRebel <202270441+DustyRebel@users.noreply.github.com>
2025-04-12 22:50:03 +0700

    .env changed, jwt installed,  auth added, role check added. tutorial server side done. need to add basket and rating without tutorial.

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const models = require('./models/models')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
-const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 const app = express()
